ab33186d209c70f8c4517c20b943c88e7a72f252
Arasjie <martijn.valkenborgh@student.pxl.be>
2021-12-27 20:21:35 +0100

    bilingual

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const englishDessertWords = [
   "Waffles",
   "Pancakes",
   "Large Pies (31cm)",
-  "Small Pies"
+  "Small Pies (13cm)"
 ]
 const englishPies = [
   "Apple Crumble",
@@ -28,6 +28,34 @@ const englishPies = [
   "Nodule Apricot",
   "Nodule Apple",
   "Nodule Pear"
+]
+
+const svenskaFormWords = [
+  "Belgisk Bageri Marlie",
+  "Använd detta formulär för att göra din beställning direkt hos oss.",
+  "Håll ett öga på vår facebooksida för månadens paj och andra nya smaker.",
+  "Namn",
+  "E-post",
+  "Telefonnummer",
+  "Ta ut datum",
+  "Extra anteckningar"
+];
+const svenskaDessertWords = [
+  "Våfflor och pannkakor",
+  "Våfflor",
+  "Pannkakor",
+  "Stora pajer (31 cm)",
+  "Små pajer (13cm)"
+]
+const svenskaPies = [
+  "Äppelsmula",
+  "Ostkaka",
+  "Tarte Tatin Äpple",
+  "Tarte Tatin Päron",
+  "Knölpaj Pudding",
+  "Knölpaj Aprikos",
+  "Knölpaj Äpple",
+  "Knölpaj Paron"
 ]
 
 //must be below app express
@@ -45,12 +73,36 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
+app.get("/handla", function(req, res) {
+  res.render("handla");
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
+
+app.get("/kontakt", function(req, res) {
+  res.render("kontakt");
+});
+
 app.get("/formenglish", function(req, res) {
-  res.render("formenglish", {
+  res.render("form", {
     form: englishFormWords,
     dessert: englishDessertWords,
     pies: englishPies
   });
+});
+
+app.get("/formsvenska", function(req, res) {
+  res.render("form", {
+    form: svenskaFormWords,
+    dessert: svenskaDessertWords,
+    pies: svenskaPies
+  });
+});
+
+app.get("/hem", function(req, res) {
+  res.render("hem");
 });
 
 app.listen(3000, function() {
